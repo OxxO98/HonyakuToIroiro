1f8cc2407a837780ae7d5ea6e6304550041f21bd
OxxO98 <hbok1119@naver.com>
2023-07-06 00:07:36 +0900

    Re

---
+++ precache-manifest.9e889be7f3b7e1ed1d5941d4410230d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f77b9265a8a626908b118cde936de8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0702aedbce43256b9783",
+    "url": "/static/css/2.741e3868.chunk.css"
+  },
+  {
+    "revision": "e96cd91a9cf3cc8d8960",
+    "url": "/static/css/main.24ec84d3.chunk.css"
+  },
+  {
+    "revision": "0702aedbce43256b9783",
+    "url": "/static/js/2.266798cc.chunk.js"
+  },
+  {
+    "revision": "d3fd67028419a7f3be60983929e35b8e",
+    "url": "/static/js/2.266798cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96cd91a9cf3cc8d8960",
+    "url": "/static/js/main.8ed218bd.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from './About';
-export { default as Home } from './HomeContainer';
-export { default as Jan } from './JanContainer';
-export { default as Thumnail } from './ThumbnailContainer';
