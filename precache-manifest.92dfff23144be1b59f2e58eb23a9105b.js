d5db4a8429f23aeb8deac48ec4524535245e479b
OxxO98 <hbok1119@naver.com>
2023-07-12 18:04:18 +0900

    asdf

---
+++ precache-manifest.92dfff23144be1b59f2e58eb23a9105b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de148b1e4dcb2430c8ea5e5169b77dc",
+    "url": "/HonyakuToIroiro/index.html"
+  },
+  {
+    "revision": "8c616cc8707e7d4dc873",
+    "url": "/HonyakuToIroiro/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "28b7a8835201d5fea6aa",
+    "url": "/HonyakuToIroiro/static/css/main.24ec84d3.chunk.css"
+  },
+  {
+    "revision": "8c616cc8707e7d4dc873",
+    "url": "/HonyakuToIroiro/static/js/2.940ef42d.chunk.js"
+  },
+  {
+    "revision": "d936b9e494a465cf60caa85a721c4ffa",
+    "url": "/HonyakuToIroiro/static/js/2.940ef42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b7a8835201d5fea6aa",
+    "url": "/HonyakuToIroiro/static/js/main.b6459480.chunk.js"
+  },
+  {
+    "revision": "e4fc1ff7c14d28457f93",
+    "url": "/HonyakuToIroiro/static/js/runtime-main.38f53526.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a0bfc59817cbc3139290ec6a7ef7ca3a.js"
+  "/HonyakuToIroiro/precache-manifest.92dfff23144be1b59f2e58eb23a9105b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HonyakuToIroiro/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
